6dca34e31a55b57842d7188899bc1a2d893d8e97
jackieleune <55492440+jackieleune@users.noreply.github.com>
2021-03-11 23:57:42 +0100

    Add files via upload

---
+++ versie 2/closed-captions/sounds.js
@@ -0,0 +1,22 @@
+// Hier kan je tijden toevoegen in secondes
+// Op die mommenten komt er een class op de body.
+sounds = [
+	0,
+	9,
+    10,
+	12,
+    13,
+	15.5,
+	17,
+	24,
+	25.8,
+    26.3,
+	31,
+	32,
+    32.9,
+    33,
+	34,
+    76,
+    77,
+	86
+];
